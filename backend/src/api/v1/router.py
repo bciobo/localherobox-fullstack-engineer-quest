9bfe0c8d1e7821f9286b821159b84e7cef2080d6
bciobo <bogdan.ciobotaru@linus.finance.com>
2022-09-14 12:07:36 +0200

    add `v1` to api

--- backend/src/api/v1/router.py
+++ backend/src/api/v1/router.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 
 from .endpoints import orders
 
-api_router = APIRouter()
+api_router = APIRouter(prefix='/v1')
 api_router.include_router(orders.router, prefix='/orders', tags=['orders'])
