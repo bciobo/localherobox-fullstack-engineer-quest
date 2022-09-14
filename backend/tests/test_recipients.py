13e7b5385d681d77f82f7daf919d81f5351a4107
bciobo <bogdan.ciobotaru@linus.finance.com>
2022-09-14 09:31:08 +0200

    add fixtures and test config

--- backend/tests/test_recipients.py
+++ backend/tests/test_recipients.py
@@ -1,37 +1,37 @@
 """lhb-backend.tests.test_recipients."""
-from sqlalchemy.engine import create_engine
-from sqlalchemy.orm import sessionmaker
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
 
-from ..src.main import app
-from ..src.database import Base
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-client = TestClient(app)
+from .utils import create_recipient
+from .fixtures import app, session, client
 
 
-def test_get_all_recipients():  # noqa
-    response = client.get('/recipients/')
+def test_get_all_recipients(
+        app: FastAPI,
+        session: Session,
+        client: TestClient,
+):
+    for i in range(1, 4):
+        create_recipient(session, i)
+    response = client.get('/api/orders/')
     assert response.status_code == 200
     recipients = response.json()
-    assert len(recipients) > 0
+    assert len(recipients) == 3
 
 
-def test_get_recipients_for_email():  # noqa
-    response = client.get('/recipients/?email=test@email.com')
+def test_get_recipients_for_email(
+        app: FastAPI,
+        session: Session,
+        client: TestClient,
+):
+    create_recipient(session)
+    response = client.get('/api/orders/?email=test@email.com')
     assert response.status_code == 200
     recipients = response.json()
     first_recipient = recipients[0]
     assert first_recipient['email'] == 'test@email.com'
-    assert first_recipient['order_number'] == '1111'
+    assert first_recipient['order_number'] == '0000'
     assert first_recipient['street'] == 'Strasse 1'
     assert first_recipient['zip_code'] == '12345'
     assert first_recipient['city'] == 'Berlin'
@@ -40,15 +40,23 @@ def test_get_recipients_for_email():  # noqa
     assert first_recipient['order_status'] == 'Order processed'
 
 
-def test_get_recipients_for_unknown_email():  # noqa
+def test_get_recipients_for_unknown_email(
+        app: FastAPI,
+        session: Session,
+        client: TestClient,
+):
     email_address = 'unknown@email.com'
-    response = client.get('/recipients/?email=%s' % email_address)
+    response = client.get('/api/orders/?email=%s' % email_address)
     assert response.status_code == 404
     assert response.json() == {'message': 'No recipients found for email: %s' % email_address}  # noqa
 
 
-def test_get_recipient_by_id():  # noqa
-    response = client.get('/recipients/1234')
+def test_get_recipient_by_id(
+        app: FastAPI,
+        session: Session,
+        client: TestClient,
+):
+    response = client.get('/api/orders/1234')
     assert response.status_code == 200
     recipient = response.json()
     assert recipient['order_number'] == '1234'
@@ -70,8 +78,12 @@ def test_get_recipient_by_id():  # noqa
     assert first_article['warehouse'] == 'Munich'
 
 
-def test_get_recipient_for_unknown_id():  # noqa
+def test_get_recipient_for_unknown_id(
+        app: FastAPI,
+        session: Session,
+        client: TestClient,
+):
     recipient_id = 9999
-    response = client.get('/recipients/%i' % recipient_id)
+    response = client.get('/api/orders/%i' % recipient_id)
     assert response.status_code == 404
     assert response.json() == {'message': 'No recipient found with number: #%i' % recipient_id}  # noqa
---
+++ backend/tests/utils.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+from ..src.models import Recipient
+
+
+def create_recipient(db: Session, i=''):
+    db_obj = Recipient(
+        order_number=f'0000{i}',
+        email=f'test@email.com{i}',
+        street=f'Strasse 1{i}',
+        zip_code='12345',
+        city='Berlin',
+        destination_country_iso3='DEU',
+        campaign_id=f'12345678{i}',
+        order_status='Order processed',
+    )
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+    return db_obj
